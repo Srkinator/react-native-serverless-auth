5fcd17b3e401ef32e7543b4483f516504cd8dc23
Srdjan Popovic <srdjan.popovic@guidefitter.com>
2020-04-20 20:36:40 +0200

    Implement verifyOneTimePassword lambda function

--- server/auth-lambda/handler.js
+++ server/auth-lambda/handler.js
@@ -1,9 +1,11 @@
 const createUser = require('./src/create-user');
 const sendOneTimePassword = require('./src/send-one-time-password');
+const verifyOneTimePassword = require('./src/verify-one-time-password');
 
 
 
 module.exports = {
   createUser,
-  sendOneTimePassword
+  sendOneTimePassword,
+  verifyOneTimePassword
 };
